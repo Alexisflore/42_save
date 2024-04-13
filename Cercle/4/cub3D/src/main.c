b4faf511be85f079806c7514a9dc83a0aec05a2d
Macbookpro <macbookpro@MacBook-Pro-di-Charlie.local>
2024-04-13 17:56:24 +0200

    close window event

--- Cercle/4/cub3D/src/main.c
+++ Cercle/4/cub3D/src/main.c
@@ -6,11 +6,18 @@
 /*   By: macbookpro <macbookpro@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 17:00:53 by macbookpro        #+#    #+#             */
-/*   Updated: 2024/04/13 17:29:51 by macbookpro       ###   ########.fr       */
+/*   Updated: 2024/04/13 17:55:48 by macbookpro       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+int	close_window_event(t_data *data)
+{
+	free_path(&data->path);
+	free_data(&data);
+	exit(0);
+}
 
 int	main(void)
 {
@@ -28,6 +35,8 @@ int	main(void)
 	mlx_win_init(data, path);
 	drawrays_3d(data);
 	mlx_hook(data->win, 02, (1L << 0), player_move_with_angle, data);
+	mlx_hook(data->win, 17, (1L << 17),
+		close_window_event, data->mlx);
 	mlx_loop(data->mlx);
 	free_path(&path);
 	free_data(&data);
